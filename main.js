bc5d8b917bab96ee607fc5fe2e34c4f3a5577f83
sizalel <teolis.rainyday@gmail.com>
2019-10-04 16:30:51 +0200

    v3

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({ disableEveryone: true });
-
+const { token, prefix } = require("./config");
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Bonjour maître");
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") msg.channel.send("Pong!");
-  if (msg.content === "everyone")
-    msg.channel.send("@everyone Salut à tous!", { disableEveryone: false });
-  if (msg.content === "noteveryone")
-    msg.channel.send("@everyone (noteveryone) Salut à tous");
+  if (msg.content.startsWith(`${prefix}ping`)) msg.channel.send("Pong!");
 });
 
-client.login("NjI5NjU5NTY4ODEyODUxMjAw.XZdAsw.huOB-jtii-zNpSasQvwf2BlUvN4");
+client.login(token);
